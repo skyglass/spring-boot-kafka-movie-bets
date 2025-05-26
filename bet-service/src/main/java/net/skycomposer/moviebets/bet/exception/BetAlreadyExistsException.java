cc69aebc589e584ef8c5172e9619c6e4c0f85a96
skyglass <skyglass2001@gmail.com>
2025-05-26 14:49:26 +0200

    - test change to trigger deployment

--- bet-service/src/main/java/net/skycomposer/moviebets/bet/exception/BetAlreadyExistsException.java
+++ bet-service/src/main/java/net/skycomposer/moviebets/bet/exception/BetAlreadyExistsException.java
@@ -3,6 +3,6 @@ package net.skycomposer.moviebets.bet.exception;
 public class BetAlreadyExistsException extends RuntimeException {
 
     public BetAlreadyExistsException(String customerId, String marketName) {
-        super(String.format("Duplicate Bet Request 15: Only one bet is allowed for customer '%s' and event '%s'", customerId, marketName));
+        super(String.format("Duplicate Bet Request 16: Only one bet is allowed for customer '%s' and event '%s'", customerId, marketName));
     }
 }
