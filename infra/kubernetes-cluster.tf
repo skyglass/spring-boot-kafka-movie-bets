cd76c58626f3a570826468cf94cc6ff4961a2100
skyglass <skyglass2001@gmail.com>
2025-05-27 21:34:34 +0200

    - decreased kubernetes virtual machine requirements

--- infra/kubernetes-cluster.tf
+++ infra/kubernetes-cluster.tf
@@ -11,7 +11,7 @@ resource "azurerm_kubernetes_cluster" "cluster" {
     default_node_pool {
         name            = "default"
         node_count      = 1
-        vm_size         = "Standard_D8s_v3"
+        vm_size         = "Standard_B8ms"
     }
 
     #
